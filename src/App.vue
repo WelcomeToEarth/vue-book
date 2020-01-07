9e905cc5892c08e30af0c7735386be4c7a0bd9a0
张飞飞 <zhangfei@xinhua.org>
2020-01-07 10:14:19 +0800

    frst vue-demo includes router element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">Book Manager</router-link> |
+    <router-link to="/home">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <br>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
